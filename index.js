6a2758fff82c802a82af3602953767ecd1d87672
Tim Uy <tim@loqu8.com>
2016-03-25 10:06:06 -0700

    show pageWorker.tab becomes false

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ exports.main = function(options, callbacks) {
 	  onAttach: function(pageWorker) {
 			pageWorker.id = Date.now();
 		  console.log('attaching pageWorker', pageWorker.id,'in tab.ids',pageWorker.tab.id);
+      console.log('pageWorker.tab: ',pageWorker.tab);
+
 			pageWorker.on('pageshow', function() {
 				console.log('showing pageWorker', pageWorker.id,'in tab.ids',pageWorker.tab.id);
+        console.log('pageWorker.tab: ',pageWorker.tab);
 			});
 			pageWorker.on('pagehide', function() {
 				console.log('hiding pageWorker', pageWorker.id,'in tab.ids',pageWorker.tab.id);
+        console.log('pageWorker.tab: ',pageWorker.tab);
 			});
 	    pageWorker.on('detach', function() {
 				console.log('detaching pageWorker', pageWorker.id,'in tab.ids',pageWorker.tab.id);
+        console.log('pageWorker.tab: ',pageWorker.tab);
 	      detachPageWorker(pageWorker);
 	    });
 	  }
